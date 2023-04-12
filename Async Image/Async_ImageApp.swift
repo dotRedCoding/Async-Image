c61ee8dba12f3aa1990b9a7f87b08663400fd2cd
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-04-12 09:34:25 -0400

    Initial Commit

---
+++ Async Image/Async_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_ImageApp.swift
+//  Async Image
+//
+//  Created by Jared on 2023-04-12.
+//
+
+import SwiftUI
+
+@main
+struct Async_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
